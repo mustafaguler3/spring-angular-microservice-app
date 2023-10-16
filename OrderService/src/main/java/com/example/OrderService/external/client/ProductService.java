5ae3d754a430335c4ca3906db8ba17870e3dc3a3
mustafaguler3 <mustafa.glr@hotmail.com>
2023-10-16 13:42:32 +0300

    circuit breaker added in order service

--- OrderService/src/main/java/com/example/OrderService/external/client/ProductService.java
+++ OrderService/src/main/java/com/example/OrderService/external/client/ProductService.java
@@ -1,11 +1,14 @@
 package com.example.OrderService.external.client;
 
+import com.example.OrderService.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external",fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/product")
 public interface ProductService {
 
@@ -13,6 +16,9 @@ public interface ProductService {
     public ResponseEntity<Void> reduceQuantity(@PathVariable("id") long productId,
                                                 @RequestParam long quantity);
 
+    default void fallback(Exception e){
+        throw new CustomException("Product service is not available","UNAVAIBLE",500);
+    }
 }
 
 
