9b75b7fe5b39edefcd5da4d0a75861cc5acd3d77
mustafaguler3 <mustafa.glr@hotmail.com>
2023-10-16 13:52:13 +0300

    rate limiter added in gateway service

--- CloudGateway/src/main/java/com/example/CloudGateway/CloudGatewayApplication.java
+++ CloudGateway/src/main/java/com/example/CloudGateway/CloudGatewayApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 
 @SpringBootApplication
@@ -14,6 +16,11 @@ public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+
+	@Bean
+	KeyResolver userKeySolver(){
+		return exchange -> Mono.just("userKey");
 	}
 
 	@Bean
