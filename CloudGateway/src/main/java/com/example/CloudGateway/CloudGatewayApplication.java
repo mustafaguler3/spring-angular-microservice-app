1baddf7adeaea3e11969c202b0d08f405ae7c2fe
mustafaguler3 <mustafa.glr@hotmail.com>
2023-10-16 13:32:59 +0300

    circuit breaker added into gateway service

--- CloudGateway/src/main/java/com/example/CloudGateway/CloudGatewayApplication.java
+++ CloudGateway/src/main/java/com/example/CloudGateway/CloudGatewayApplication.java
@@ -1,7 +1,13 @@
 package com.example.CloudGateway;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class CloudGatewayApplication {
@@ -10,4 +16,28 @@ public class CloudGatewayApplication {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer(){
+		return factory -> factory.configureDefault(
+				id -> new Resilience4JConfigBuilder(id).circuitBreakerConfig(CircuitBreakerConfig.ofDefaults()).build()
+		);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
